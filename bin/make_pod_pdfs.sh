255b3f76662abc41a7e247775df1c39cc48d75ba
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-04-10 19:52:30 +0200

    changed package contents for PDFs

--- bin/make_pod_pdfs.sh
+++ bin/make_pod_pdfs.sh
@@ -2,19 +2,19 @@
 
 echo "--- Catalyst ---"
 rm -f ~/Desktop/catalyst.pdf
-convert_pod2pdf.pl -v -p Catalyst -p CatalystX -f ~/Desktop/catalyst.pdf
+convert_pod2pdf.pl -v -p Catalyst -p CatalystX -p HTML::FormFu -f ~/Desktop/catalyst.pdf
 
 echo "--- Moose ---"
 rm -f ~/Desktop/moose.pdf
-convert_pod2pdf.pl -v -p Moose -p MooseX -f ~/Desktop/moose.pdf
+convert_pod2pdf.pl -v -p Moose -p MooseX -p Class::MOP -f ~/Desktop/moose.pdf
 
 echo "--- Dbix::Class ---"
 rm -f ~/Desktop/dbix_class.pdf
-convert_pod2pdf.pl -v -p DBIx::Class -f ~/Desktop/dbix_class.pdf
+convert_pod2pdf.pl -v -p DBIx::Class -p SQL::Abstract -p SQL::Translator -f ~/Desktop/dbix_class.pdf
 
 echo "--- Plack ---"
 rm -f ~/Desktop/plack.pdf
-convert_pod2pdf.pl -v -p Plack -f ~/Desktop/plack.pdf
+convert_pod2pdf.pl -v -p Plack -p PSGI -f ~/Desktop/plack.pdf
 
 echo "--- Mojo* ---"
 rm -f ~/Desktop/mojo.pdf
