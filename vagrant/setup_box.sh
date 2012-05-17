01a08857444b7ae2ce8fa5a6bc1d00f0aa3f8acd
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-05-17 13:33:41 +0200

    added vagrant ideas

--- vagrant/setup_box.sh
+++ vagrant/setup_box.sh
@@ -3,7 +3,6 @@
 # initially setup a new Ubuntu box
 # must run as root
 #
-# 
 
 echo "Setting Time Zone..."
 echo "Europe/Berlin" > /etc/timezone
@@ -19,4 +18,13 @@ apt-get -y install nginx-full postgresql-9.1 postgresql-client-9.1 redis-server 
                    libpng12-0 libpng12-dev \
                    libxml2 libxml2-dev \
                    curl
+
+echo "manipulating pg_hba.conf"
+echo "local all all trust" >> /etc/postgresql/9.1/main/pg_hba.conf
+echo "host all all 127.0.0.1/32 trust" >> /etc/postgresql/9.1/main/pg_hba.conf
+echo "host all all ::1/128 trust" >> /etc/postgresql/9.1/main/pg_hba.conf
+
+echo "enabling sudo for every admin"
+sed -s 's/^\(%admin.*\)$/# OFF: # \1\n%admin ALL=NOPASSWD: ALL/' -i .bak /etc/sudoers
+service sudo restart
 
