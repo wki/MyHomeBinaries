5433056ad8e86bcbec628e02f631efb5ff64d1ec
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-06-13 00:16:13 +0200

    added TODOs for perlbrew

--- vagrant/sample_puppet_things/modules/perlbrew/manifests/init.pp
+++ vagrant/sample_puppet_things/modules/perlbrew/manifests/init.pp
@@ -2,15 +2,21 @@ class perlbrew($user = 'vagrant') {
   
   ### FIXME: can we ensure user is present?
   
-  exec { 'wget':
+  ### FIXME: correctoy depend on wget
+  
+  exec { 'install':
     command => '/usr/bin/wget -q -O - http://install.perlbrew.pl | /bin/bash',
     creates => "/home/$user/perl5/perlbrew/bin/perlbrew",
     cwd => "/home/$user",
     # group => $user,
     user => $user,
     
+    # require => Package['wget'],
+    
     # notify { Exec[setup]: }
   }
+  
+  ### TODO: perlbrew-init, perlbrew-install-cpanm
   
   # exec { 'setup':
   #   command => '/bin/echo "setup would run"',
