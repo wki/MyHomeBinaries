43633f9757f945c95627af593afd6c0899a6a5e0
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-06-22 23:23:11 +0200

    added installing missing modules as preparation of provisioning

--- vagrant/moose_provisioning_lib/lib/Provision/Prepare.pm
+++ vagrant/moose_provisioning_lib/lib/Provision/Prepare.pm
@@ -5,6 +5,7 @@ use feature ':5.10';
 use FindBin;
 use Cwd 'abs_path';
 use File::Find;
+use Module::CoreList;
 use HTTP::Lite;  ### must be included, but is PP and only 1 File
 
 #
@@ -15,19 +16,20 @@ use HTTP::Lite;  ### must be included, but is PP and only 1 File
 #   - all modules required by this set of modules installed
 #
 
-our $DEBUG = 1;
+our $DEBUG       = 1;
+
 our $CPAN_MIRROR = 'http://cpan.noris.de';
 our $PACKAGE_URL = "$CPAN_MIRROR/modules/02packages.details.txt.gz";
 our $AUTHOR_URL  = "$CPAN_MIRROR/authors/id";
 
-our $root_dir  = abs_path($FindBin::Bin);
-our $lib       = "$root_dir/lib";
-our $local     = "$root_dir/local";
-our $local_lib = "$local/lib";
-our $perl_lib  = "$local/lib/perl5";
-our $local_bin = "$local/bin";
-our $cpanm     = "$local/bin/cpanm";
-our $local_tmp = "$local/tmp";
+our $root_dir    = abs_path($FindBin::Bin);
+our $lib         = "$root_dir/lib";
+our $local       = "$root_dir/local";
+our $local_lib   = "$local/lib";
+our $perl_lib    = "$local/lib/perl5";
+our $local_bin   = "$local/bin";
+our $cpanm       = "$local/bin/cpanm";
+our $local_tmp   = "$local/tmp";
 
 our $packages;
 our %distribution_for;
@@ -38,10 +40,9 @@ sub import {
     create_local()  if !-d $local;
     install_cpanm() if !-x $cpanm;
     install_missing_modules();
-    # ...
-    
+
     eval "use lib '$local_lib'";
-    
+
     say "Provision::Prepare done" if $DEBUG;
 }
 
@@ -56,66 +57,79 @@ sub create_local {
 
 sub install_cpanm {
     load_package_file();
-    
+
     $packages =~ m{App::cpanminus\s+([0-9.]+)\s+(M/MI/MIYAGAWA/.*?\.gz)}xms
         or die 'could not find cpanm in package list, stopping';
     my $version = $1;
     my $path = $2;
     say "CPANM: version=$version, path=$path" if $DEBUG;
-    
+
     my $tar_gz = get_via_http("$AUTHOR_URL/$path")
         or die 'could not load cpanm .tar.gz';
-    
+
     write_to($tar_gz, '>', "$local_tmp/cpanm.tar.gz");
-    
+
     system "tar xfz $local_tmp/cpanm.tar.gz -O --include '*/bin/cpanm' > $cpanm"
         and die 'could not extract .tar.gz';
-    
+
     chmod 0755, $cpanm;
-    
+
     say 'installed cpanm' if $DEBUG;
 }
 
 sub install_missing_modules {
     load_package_file();
-    
+
     say 'collecting  modules...' if $DEBUG;
-    
+
     foreach my $line (split qr{[\r\n]+}xms, $packages) {
         next if $line !~ m{\A (\S+) .*? (\S+) \s*\z}xms;
         $distribution_for{$1} = $2;
     }
-    
+
     my %install_module;
     find(sub {
         return if !-f;
-        
+
         my $content = read_from('<', $_);
         foreach my $module ($content =~ m{^\s* use \s+ ([\w:]+)}xmsg) {
             next if !exists $distribution_for{$module};
             $install_module{$module}++;
         }
     }, $lib);
-    
-    say for sort keys %install_module;
+
+    foreach my $module (sort keys %install_module) {
+        my $first_release = Module::CoreList->first_release($module)
+            and next;
+        say "checking $module..." if $DEBUG;
+
+        eval "use $module";
+        if ($@) {
+            ### TODO: remove -n switch
+            system "perl '$cpanm' -L '$local' -n '$module' >/dev/null 2>/dev/null"
+                and die "error loading module '$module'";
+        } else {
+            say "...$module is present" if $DEBUG;
+        }
+    }
 }
 
 sub load_package_file {
     return if $packages;
-    
+
     my $package_file = "$local_tmp/packages.gz";
-    
+
     if (!-f $package_file || -M $package_file > 1) {
         my $packages_gz = get_via_http($PACKAGE_URL);
         write_to($packages_gz, '>', "$local_tmp/packages.gz");
     }
-    
+
     $packages = read_from('-|', "gunzip --to-stdout '$package_file'");
 }
 
 sub write_to {
     my ($content, $mode, $file) = @_;
-    
+
     open my $fh, $mode, $file
         or die "cannot open $file for writing";
     print $fh $content;
@@ -125,23 +139,23 @@ sub write_to {
 
 sub read_from {
     my ($mode, $file) = @_;
-    
+
     local $/ = undef;
-    
+
     open my $fh, $mode, $file
         or die "cannot open $file";
     my $content = <$fh>;
     close $fh
         or die "cannot read from $file";
-    
+
     return $content;
 }
 
 sub get_via_http {
     my $url = shift;
-    
+
     say "get_via_http: '$url'";
-    
+
     my $http = HTTP::Lite->new;
     my $req = $http->request($url)
         or die "Unable to get document: $!";
