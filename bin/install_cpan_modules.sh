de09ab3d4c4942bd40b2b2a200a104e04b055f39
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2013-05-20 09:39:32 +0200

    piped distributions into cpan for mass installation

--- bin/install_cpan_modules.sh
+++ bin/install_cpan_modules.sh
@@ -10,48 +10,46 @@
 
 # curl -L http://smartcd.org/install | bash
 
-for m in Bundle::CPAN CPAN::Mini Carton \
-         Modern::Perl \
-         Imager Imager::File::GIF Imager::File::JPEG Imager::File::PNG \
-         Class::Trigger \
-         DBD::Pg DateTime::Format::Pg \
-         DBIx::Class DBIx::Class::Schema::Loader DBIx::Class::Candy DBIx::Class::Helpers \
-         DBIx::Class::DeploymentHandler DBIx::Class::Migration \
-         Moose MooseX::Types MooseX::NonMoose \
-         MooseX::Getopt MooseX::SimpleConfig \
-         MooseX::Types::Path::Class MooseX::Types::Stringlike \
-         MooseX::Types::Path::Class::MoreCoercions \
-         Moo Role::Tiny \
-         Catalyst::Runtime Catalyst::Devel \
-         Catalyst::Plugin::ConfigLoader \
-         Catalyst::Plugin::Unicode Catalyst::Plugin::I18N \
-         Catalyst::Plugin::Authentication Catalyst::Plugin::Authorization::Roles \
-         Catalyst::Authentication::Credential::HTTP Catalyst::Plugin::Authentication::Credential::HTTP \
-         Catalyst::Plugin::Session \
-         Catalyst::Plugin::Static::Simple \
-         Catalyst::Plugin::StatusMessage \
-         Catalyst::View::ByCode Catalyst::View::Email Catalyst::View::JSON \
-         Catalyst::Model::DBIC::Schema Catalyst::Model::Adaptor \
-         Catalyst::Controller::Combine Catalyst::Controller::Imager Catalyst::View::ByCode \
-         Catalyst::Controller::REST \
-         HTML::FormFu HTML::FormFu::Model::DBIC Catalyst::Controller::HTML::FormFu \
-         Dancer \
-         Web::Simple HTTP::Tiny IO::Socket::SSL\
-         Template::Simple \
-         Mojolicious \
-         Plack \
-         Dist::Zilla Dist::Zilla::Plugin::VersionFromModule Dist::Zilla::Plugin::Inject \
-         Dist::Zilla::Plugin::Git::Check \
-         Test::More Test::Most Test::Exception Test::DBIx::Class \
-         XML::LibXML \
-         JONALLEN/pod2pdf-0.42.tar.gz \
-         IPC::System::Simple IPC::Run3 \
-         Hash::Merge \
-         Try::Tiny \
-         App::Ack
-do
-    cpanm --mirror ~/minicpan --mirror-only $m $*
-done
+echo    Bundle::CPAN CPAN::Mini Carton \
+        Modern::Perl \
+        Imager Imager::File::GIF Imager::File::JPEG Imager::File::PNG \
+        Class::Trigger \
+        DBD::Pg DateTime::Format::Pg \
+        DBIx::Class DBIx::Class::Schema::Loader DBIx::Class::Candy DBIx::Class::Helpers \
+        DBIx::Class::DeploymentHandler DBIx::Class::Migration \
+        Moose MooseX::Types MooseX::NonMoose \
+        MooseX::Getopt MooseX::SimpleConfig \
+        MooseX::Types::Path::Class MooseX::Types::Stringlike \
+        MooseX::Types::Path::Class::MoreCoercions \
+        Moo Role::Tiny \
+        Catalyst::Runtime Catalyst::Devel \
+        Catalyst::Plugin::ConfigLoader \
+        Catalyst::Plugin::Unicode Catalyst::Plugin::I18N \
+        Catalyst::Plugin::Authentication Catalyst::Plugin::Authorization::Roles \
+        Catalyst::Authentication::Credential::HTTP Catalyst::Plugin::Authentication::Credential::HTTP \
+        Catalyst::Plugin::Session \
+        Catalyst::Plugin::Static::Simple \
+        Catalyst::Plugin::StatusMessage \
+        Catalyst::View::ByCode Catalyst::View::Email Catalyst::View::JSON \
+        Catalyst::Model::DBIC::Schema Catalyst::Model::Adaptor \
+        Catalyst::Controller::Combine Catalyst::Controller::Imager Catalyst::View::ByCode \
+        Catalyst::Controller::REST \
+        HTML::FormFu HTML::FormFu::Model::DBIC Catalyst::Controller::HTML::FormFu \
+        Dancer \
+        Web::Simple HTTP::Tiny IO::Socket::SSL\
+        Template::Simple \
+        Mojolicious \
+        Plack \
+        Dist::Zilla Dist::Zilla::Plugin::VersionFromModule Dist::Zilla::Plugin::Inject \
+        Dist::Zilla::Plugin::Git::Check \
+        Test::More Test::Most Test::Exception Test::DBIx::Class \
+        XML::LibXML \
+        JONALLEN/pod2pdf-0.42.tar.gz \
+        IPC::System::Simple IPC::Run3 \
+        Hash::Merge \
+        Try::Tiny \
+        App::Ack \
+    | cpanm --mirror ~/minicpan --mirror-only $m $*
 
 # install plenv if wanted
 # git clone git://github.com/tokuhirom/plenv.git ~/.plenv
